ae04b28d4eaf03b7f5c3c1c11a4c83706c03c35a
detriste <detriste2018@gmail.com>
2026-04-28 14:33:37 -0300

    mudanças

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use('/api', require('./rotas/statusRotas'));
 // POST /api/ferramentas/:id/manutencao
 // POST /api/ferramentas/:id/disponibilizar
 
+
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');
 });
